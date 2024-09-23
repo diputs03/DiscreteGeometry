38a3e3f38de30452f71a37a46229d883c9a28f08
Han Li <88031193+diputs03@users.noreply.github.com>
2024-09-23 19:40:10 +0800

    Update 2DMovingTopoGraph.py

--- 2DMovingTopoGraph.py
+++ 2DMovingTopoGraph.py
@@ -25,14 +25,17 @@ for c in range(num_segments):
     k1.append(int(np.random.rand() * 100) % n)
     k2.append(int(np.random.rand() * 100) % n)
 
+# The animate function is essential for time relevent animations
 def animate(t): 
     for i in range(n):
         x[i] += np.random.rand() - 0.5
         y[i] += np.random.rand() - 0.5
+# The animation is plotting uppon the current graph, so we need to clear the current graph
     plt.cla()
     plt.scatter(x, y)
     plt.plot([x[k1], x[k2]], [y[k1], y[k2]], marker = 'o', color = 'blue')
 
+# gcf is get-current-figure, interval is the time elapse of each change
 ani = FuncAnimation(plt.gcf(), animate, interval = 0.01)
 
 plt.show()
